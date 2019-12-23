3e0e2c0b1a6b9f1909ef49000e1a666612ca83cf
cloudemprise <dh.info@posteo.net>
2019-12-23 18:47:42 +0000

    Can not use nested ec2 launch template with userdata/metadata & creation policy. Have to create ec2 as stand-alone (also nested)

--- dh.cform.openvpn.sh
+++ dh.cform.openvpn.sh
@@ -4,7 +4,7 @@ aws s3 cp dh.cform.openvpn.yaml s3://dh.cform-templates/openvpn/
 aws s3 cp dh.cform.openvpn-vpc.yaml s3://dh.cform-templates/openvpn/
 aws s3 cp dh.cform.openvpn-nacl.yaml s3://dh.cform-templates/openvpn/
 aws s3 cp dh.cform.openvpn-sg.yaml s3://dh.cform-templates/openvpn/
-aws s3 cp dh.cform.openvpn-launch-priv.yaml s3://dh.cform-templates/openvpn/
-aws s3 cp dh.cform.openvpn-launch-pub.yaml s3://dh.cform-templates/openvpn/
+#aws s3 cp dh.cform.openvpn-ec2-priv.yaml s3://dh.cform-templates/openvpn/
+aws s3 cp dh.cform.openvpn-ec2-pub.yaml s3://dh.cform-templates/openvpn/
 
 aws cloudformation create-stack --stack-name OpenVPNTest01 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test
