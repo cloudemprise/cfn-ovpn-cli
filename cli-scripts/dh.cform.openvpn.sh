0397c6e27b21dfae09687ec891fbd3acfd66c823
cloudemprise <dh.info@posteo.net>
2019-12-26 19:40:26 +0000

    Include restrictive bucket policies for cloudformation/s3 & ec2/s3 access. File retrival from S3 from template implemented. Restrictive iptables rules configured

--- cli-scripts/dh.cform.openvpn.sh
+++ cli-scripts/dh.cform.openvpn.sh
@@ -13,8 +13,12 @@ aws s3 cp dh.cform.openvpn-ec2-pub.yaml $S3_TEMPLATE_LOCATION
 
 #Compress & Upload iptables script to S3
 #aws s3 cp dh.cform.openvpn-ec2-pub-iptables.tar.gz $S3_IPTABLES_LOCATION
+#tar -cf - dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - s3://dh.scripts/iptables/dh.cform.openvpn-ec2-pub-iptables.sh.tar
 #gzip -c dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-pub-iptables.sh.gz
+cd iptables-scripts/
+tar -zcf - dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-pub-iptables.sh.tar.gz
+cd ..
 
 
 #Create cloudformation stack
-aws cloudformation create-stack --stack-name OpenVPNTest01 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test --on-failure DO_NOTHING
+aws cloudformation create-stack --stack-name OpenVPNTest02 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test --on-failure DO_NOTHING
