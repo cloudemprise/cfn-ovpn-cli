7a8cc2a6e3ffba9496f59c30d02677424871d76b
cloudemprise <dh.info@posteo.net>
2019-12-27 21:25:47 +0000

    IPtables and associated logs installed and configured. OpenVPN server configurations uploaded to S3. sshd hardening config also uploaded to S3. Removed private NAT gateway just for developement

--- cli-scripts/dh.cform.openvpn.sh
+++ cli-scripts/dh.cform.openvpn.sh
@@ -2,6 +2,10 @@
 
 S3_TEMPLATE_LOCATION="s3://dh.cform-templates/openvpn/"
 S3_IPTABLES_LOCATION="s3://dh.scripts/iptables/"
+S3_SSH_LOCATION="s3://dh.scripts/ssh/"
+S3_OPENVPN_LOCATION="s3://dh.scripts/openvpn/"
+
+
 
 #Upload latest Nested Templates to S3
 aws s3 cp dh.cform.openvpn.yaml $S3_TEMPLATE_LOCATION
@@ -12,13 +16,20 @@ aws s3 cp dh.cform.openvpn-ec2-pub.yaml $S3_TEMPLATE_LOCATION
 #aws s3 cp dh.cform.openvpn-ec2-priv.yaml $S3_TEMPLATE_LOCATION
 
 #Compress & Upload iptables script to S3
-#aws s3 cp dh.cform.openvpn-ec2-pub-iptables.tar.gz $S3_IPTABLES_LOCATION
-#tar -cf - dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - s3://dh.scripts/iptables/dh.cform.openvpn-ec2-pub-iptables.sh.tar
-#gzip -c dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-pub-iptables.sh.gz
 cd iptables-scripts/
 tar -zcf - dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-pub-iptables.sh.tar.gz
 cd ..
 
+#Compress & Upload sshd hardening script to S3
+cd ssh-scripts/
+tar -zcf - dh.cform.openvpn-ec2-pub-ssh.sh | aws s3 cp - ${S3_SSH_LOCATION}dh.cform.openvpn-ec2-pub-ssh.sh.tar.gz
+cd ..
+
+#Compress & Upload openvpn server configs to S3
+cd openvpn-configs/
+tar -zcf - dh.vpn.svr.udp1194.conf | aws s3 cp - ${S3_OPENVPN_LOCATION}dh.vpn.svr.udp1194.conf.tar.gz
+tar -zcf - dh.vpn.svr.tcp1194.conf | aws s3 cp - ${S3_OPENVPN_LOCATION}dh.vpn.svr.tcp1194.conf.tar.gz
+cd ..
 
 #Create cloudformation stack
 aws cloudformation create-stack --stack-name OpenVPNTest02 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test --on-failure DO_NOTHING
