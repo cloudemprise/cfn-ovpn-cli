053e41ff0ed9ae22ad4c44554ef7fba1d3d666c6
cloudemprise <cloudemprise@users.noreply.github.com>
2020-01-01 22:30:22 +0000

    Auto yum security updates. Yum priorities set. PKI signing requests generated. Other misc stuff implemented. Private Certificate Authority EC2 instance provisioned and hardedned.

--- cli-scripts/dh.cform.openvpn.sh
+++ cli-scripts/dh.cform.openvpn.sh
@@ -14,16 +14,19 @@ aws s3 cp dh.cform.openvpn-vpc.yaml $S3_TEMPLATE_LOCATION
 aws s3 cp dh.cform.openvpn-nacl.yaml $S3_TEMPLATE_LOCATION
 aws s3 cp dh.cform.openvpn-sg.yaml $S3_TEMPLATE_LOCATION
 aws s3 cp dh.cform.openvpn-ec2-pub.yaml $S3_TEMPLATE_LOCATION
+aws s3 cp dh.cform.openvpn-ec2-priv.yaml $S3_TEMPLATE_LOCATION
 #aws s3 cp dh.cform.openvpn-ec2-priv.yaml $S3_TEMPLATE_LOCATION
 
 #Compress & Upload iptables script to S3
 cd iptables-scripts/
 tar -zcf - dh.cform.openvpn-ec2-pub-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-pub-iptables.sh.tar.gz
+tar -zcf - dh.cform.openvpn-ec2-priv-iptables.sh | aws s3 cp - ${S3_IPTABLES_LOCATION}dh.cform.openvpn-ec2-priv-iptables.sh.tar.gz
 cd ..
+
 
 #Compress & Upload sshd hardening script to S3
 cd ssh-scripts/
-tar -zcf - dh.cform.openvpn-ec2-pub-ssh.sh | aws s3 cp - ${S3_SSH_LOCATION}dh.cform.openvpn-ec2-pub-ssh.sh.tar.gz
+tar -zcf - dh.cform.openvpn-ec2-harden-ssh.sh | aws s3 cp - ${S3_SSH_LOCATION}dh.cform.openvpn-ec2-harden-ssh.sh.tar.gz
 cd ..
 
 #Compress & Upload openvpn server configs to S3
@@ -36,4 +39,4 @@ tar -zcf - vars.* | aws s3 cp - ${S3_EASYRSA_LOCATION}dh.cform.easyrsa.vars.tar.
 cd ..
 
 #Create cloudformation stack
-aws cloudformation create-stack --stack-name OpenVPNTest02 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test --on-failure DO_NOTHING
+aws cloudformation create-stack --stack-name OpenVPNTest01 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --tags Key=Name,Value=OpenVPN-Test --on-failure DO_NOTHING
