a5e9e8a963035caef651aac81bda61a3eaa5bf29
cloudemprise <cloudemprise@users.noreply.github.com>
2020-03-18 15:10:15 +0000

    Stage 1 & 2 Complete. Auto cfn AMI creation of complete Public Openvpn Server with Private PKI. Result: VPC, SG, NACL and 2 AMI comprising the PKI and Openvpn server.

--- dh.cform.openvpn.sh
+++ dh.cform.openvpn.sh
@@ -8,16 +8,21 @@ S3_EASYRSA_LOCATION="s3://dh.scripts/easyrsa/openvpn/gen-reqs/"
 
 BUILDSTAGE="Stage0"
 
-STACK_NAME="openvpn-set1-6"
+STACK_NAME="openvpn-set1-4"
 STACK_ID=""
 
 # Instance IDs of Stack1 Public/Private
-INSTANCE_ID_PUB=""
+INSTANCE_ID_PUB1=""
+INSTANCE_ID_PUB2=""
 INSTANCE_ID_PRIV=""
 
 AMI_CURRENT=""
-AMI_IMAGE_PUB=""
+AMI_IMAGE_PUB1=""
+AMI_IMAGE_PUB2=""
 AMI_IMAGE_PRIV=""
+
+AWS_ACC_ID=""
+SNAPSHOT_PUB1=""
 
 #------------
 #Upload latest Nested Templates to S3
@@ -52,35 +57,120 @@ echo "The lastest Amazon Linux 2 AMI : $AMI_CURRENT"
 #------------
 # Create Stage0 Stack
 STACK_ID=$(aws cloudformation create-stack --stack-name $STACK_NAME --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=LatestAmi,ParameterValue=$AMI_CURRENT --tags Key=Name,Value=openvpn-stage0 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --on-failure DO_NOTHING --output text)
-echo "stack ID : $STACK_ID"
+echo "Cloudformation Stack ID : $STACK_ID"
 # Wait for Stage0 to complete
-aws cloudformation wait stack-create-complete --stack-name $STACK_ID || echo "Error Stack Wait Failed"
-echo "Stage0 Stack Creation Complete : $STACK_ID"
+if (aws cloudformation wait stack-create-complete --stack-name $STACK_ID)
+then echo "Stage0 Stack Update is now Complete : $STACK_ID"
+else echo "Error: Stack Wait Update Failed : $STACK_ID"
+fi
 
 #------------
 # Update Stack with Stage1 parameters
 BUILDSTAGE="Stage1"
-aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=LatestAmi,ParameterValue=$AMI_CURRENT --tags Key=Name,Value=openvpn-stage1 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml
-echo "Stage 1 Stack Update Initiated"
+aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=LatestAmi,ParameterValue=$AMI_CURRENT --tags Key=Name,Value=openvpn-stage1 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml > /dev/null
+echo "Stage1 Stack Update has now been Initiated..."
 # Wait for Stage1 Update to complete
-aws cloudformation wait stack-update-complete --stack-name $STACK_ID || echo "Error: Stack Wait Update Failed"
-echo "Stage1 Stack Update Complete : $STACK_ID"
+if (aws cloudformation wait stack-update-complete --stack-name $STACK_ID)
+then echo "Stage1 Stack Update is now Complete : $STACK_ID"
+else echo "Error: Stack Wait Update Failed : $STACK_ID"
+fi
 
 #------------
 # Grab the IDs of the ec2 instances for further processing
-INSTANCE_ID_PUB=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPubStage1'].OutputValue")
-echo "Public Instance ID is : $INSTANCE_ID_PUB"
+INSTANCE_ID_PUB1=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPubStage1'].OutputValue")
+echo "Public Stage1 Instance ID is : $INSTANCE_ID_PUB1"
 INSTANCE_ID_PRIV=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPrivate'].OutputValue")
 echo "Private Instance ID is : $INSTANCE_ID_PRIV"
 
 #------------
 # Validity Check Here.
 # Wait for instance status ok
-aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PUB &
+aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PUB1 &
 P1=$!
 aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PRIV &
 P2=$!
 wait $P1 $P2
-echo "Public Instance State: Ok"
-echo "Private Instance State: Ok"
+echo "Public Stage1 Instance State: Ok..."
+echo "Private Instance State: Ok..."
+
+#------------
+# Create IMAGE AMIs
+AMI_IMAGE_PUB1=$(aws ec2 create-image --instance-id $INSTANCE_ID_PUB1 --name $(echo "openvpn-public-1-$INSTANCE_ID_PUB1") --description "Public OpenVPN AMI" --output text)
+echo "Public Stage1 AMI creation has now been initiated : $AMI_IMAGE_PUB1"
+AMI_IMAGE_PRIV=$(aws ec2 create-image --instance-id $INSTANCE_ID_PRIV --name $(echo "openvpn-private-$INSTANCE_ID_PRIV") --description "Private OpenVPN AMI" --output text)
+echo "Private AMI creation has now been initiated : $AMI_IMAGE_PRIV"
+
+# Wait for new AMIs to become available
+aws ec2 wait image-available --image-ids $AMI_IMAGE_PUB1 &
+P1=$!
+aws ec2 wait image-available --image-ids $AMI_IMAGE_PRIV &
+P2=$!
+wait $P1 $P2
+echo "Public Stage1 AMI is now available : $AMI_IMAGE_PUB1 "
+echo "Private AMI is now available : $AMI_IMAGE_PRIV"
+
+# Terminate the instances - no longer needed.
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID_PUB1 $INSTANCE_ID_PRIV > /dev/null
+echo "Stage1 Instances have now terminated..."
+
+#------------
+# Update Stack with Stage2 parameters
+BUILDSTAGE="Stage2"
+aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=LatestAmi,ParameterValue=$AMI_IMAGE_PUB1 --tags Key=Name,Value=openvpn-stage2 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml > /dev/null
+echo "Stage2 Stack Update has now been Initiated..."
+# Wait for Stage2 Update to complete
+if (aws cloudformation wait stack-update-complete --stack-name $STACK_ID)
+then echo "Stage2 Stack Update is now Complete : $STACK_ID"
+else echo "Error: Stack Wait Update Failed : $STACK_ID"
+fi
+
+#------------
+# Grab the IDs of the ec2 instances for further processing
+INSTANCE_ID_PUB2=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPubStage2'].OutputValue")
+echo "Public Stage2 Instance ID is : $INSTANCE_ID_PUB2"
+
+#------------
+# Validity Check Here.
+# Wait for instance status ok
+aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PUB2 &
+P1=$!
+wait $P1
+echo "Public Stage2 Instance State: Ok..."
+
+#------------
+# Create IMAGE AMIs
+AMI_IMAGE_PUB2=$(aws ec2 create-image --instance-id $INSTANCE_ID_PUB2 --name $(echo "openvpn-public-2-$INSTANCE_ID_PUB2") --description "Public 2 OpenVPN AMI" --output text)
+echo "Public Stage2 AMI creation has now been initiated : $AMI_IMAGE_PUB2"
+# Wait for new AMIs to become available
+aws ec2 wait image-available --image-ids $AMI_IMAGE_PUB2 &
+P1=$!
+wait $P1
+echo "Public Stage1 AMI is now available : $AMI_IMAGE_PUB2"
+
+# Terminate Public Stage2 instance - no longer needed.
+aws ec2 terminate-instances --instance-ids $INSTANCE_ID_PUB2 > /dev/null
+echo "Stage2 Instances have now terminated..."
+
+#------------
+# Delete AMI + snapshot of Stage1
+# Get Account ID
+AWS_ACC_ID=$(aws sts get-caller-identity --query Account --output text)
+echo "Account ID : $AWS_ACC_ID"
+
+# Get snapshot ID Public Stage1 AMI
+SNAPSHOT_PUB1=$(aws ec2 describe-images --image-ids $AMI_IMAGE_PUB1 --query "Images[].BlockDeviceMappings[].Ebs[].SnapshotId" --output text)
+echo "Public Stage1 Snapshot ID : $SNAPSHOT_PUB1"
+
+# Deregister Public Stage1 image
+if (aws ec2 deregister-image --image-id $AMI_IMAGE_PUB1)
+then echo "Deregistering Public Stage1 AMI : $AMI_IMAGE_PUB1"
+else echo "echo "Error: Failed to Deregister $AMI_IMAGE_PUB1""
+fi
+
+# Deleting Public Stage1 snapshot
+if (aws ec2 delete-snapshot --snapshot-id $SNAPSHOT_PUB1)
+then echo "Deleting Public Stage1 AMI Snapshot : $SNAPSHOT_PUB1"
+else echo "Error: Failed to Delete Snapshot $AMI_IMAGE_PUB1"
+fi
+
 
