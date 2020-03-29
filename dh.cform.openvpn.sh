daecedc240f35616301c114bbc384abc375c2d64
cloudemprise <cloudemprise@users.noreply.github.com>
2020-03-29 11:41:36 +0000

    Openvpn Server with ASG. Fixed iptables/firewalld confict bug. Fixed systemd configuration typos. Rearranged cfn-init code blocks to avoid conflicts.

--- dh.cform.openvpn.sh
+++ dh.cform.openvpn.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#!! COMMENT BEGIN
+: <<'END'
+#!! COMMENT BEGIN
+
+#!! COMMENT END
+END
+#!! COMMENT END
+
 S3_TEMPLATE_LOCATION="s3://dh.cform-templates/openvpn/"
 S3_IPTABLES_LOCATION="s3://dh.scripts/iptables/"
 S3_SSH_LOCATION="s3://dh.scripts/ssh/"
@@ -8,7 +16,7 @@ S3_EASYRSA_LOCATION="s3://dh.scripts/easyrsa/openvpn/gen-reqs/"
 
 BUILDSTAGE="Stage0"
 
-STACK_NAME="openvpn-set1-3"
+STACK_NAME="openvpn-set1-8"
 STACK_ID=""
 
 # Instance IDs of Stack1 Public/Private
@@ -26,6 +34,8 @@ SNAPSHOT_PUB1=""
 
 #------------
 #Upload latest Nested Templates to S3
+echo "Uploading Cloudformation Templates to S3"
+echo "Location: <$S3_TEMPLATE_LOCATION>"
 aws s3 cp cfn-templates/dh.cform.openvpn.yaml $S3_TEMPLATE_LOCATION
 aws s3 cp cfn-templates/dh.cform.openvpn-vpc.yaml $S3_TEMPLATE_LOCATION
 aws s3 cp cfn-templates/dh.cform.openvpn-nacl.yaml $S3_TEMPLATE_LOCATION
@@ -59,29 +69,33 @@ echo "The lastest Amazon Linux 2 AMI : $AMI_CURRENT"
 
 #------------
 # Create Stage0 Stack
+echo "$BUILDSTAGE"
 STACK_ID=$(aws cloudformation create-stack --stack-name $STACK_NAME --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=CurrentAmi,ParameterValue=$AMI_CURRENT --tags Key=Name,Value=openvpn-stage0 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml --on-failure DO_NOTHING --output text)
+echo "$BUILDSTAGE Stack has now been Initiated..."
 echo "Cloudformation Stack ID : $STACK_ID"
 # Wait for Stage0 to complete
 if (aws cloudformation wait stack-create-complete --stack-name $STACK_ID)
-then echo "Stage0 Stack Update is now Complete : $STACK_ID"
+then echo "$BUILDSTAGE Stack Update is now Complete : $STACK_ID"
 else echo "Error: Stack Wait Update Failed : $STACK_ID"
 fi
+
 
 #------------
 # Update Stack with Stage1 parameters
 BUILDSTAGE="Stage1"
+echo "$BUILDSTAGE"
 aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=CurrentAmi,ParameterValue=$AMI_CURRENT --tags Key=Name,Value=openvpn-stage1 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml > /dev/null
-echo "Stage1 Stack Update has now been Initiated..."
+echo "$BUILDSTAGE Stack has now been Initiated..."
 # Wait for Stage1 Update to complete
 if (aws cloudformation wait stack-update-complete --stack-name $STACK_ID)
-then echo "Stage1 Stack Update is now Complete : $STACK_ID"
+then echo "$BUILDSTAGE Stack Update is now Complete : $STACK_ID"
 else echo "Error: Stack Wait Update Failed : $STACK_ID"
 fi
 
 #------------
 # Grab the IDs of the ec2 instances for further processing
 INSTANCE_ID_PUB1=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPubStage1'].OutputValue")
-echo "Public Stage1 Instance ID is : $INSTANCE_ID_PUB1"
+echo "Public $BUILDSTAGE Instance ID is : $INSTANCE_ID_PUB1"
 INSTANCE_ID_PRIV=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPrivate'].OutputValue")
 echo "Private Instance ID is : $INSTANCE_ID_PRIV"
 
@@ -93,13 +107,13 @@ P1=$!
 aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PRIV &
 P2=$!
 wait $P1 $P2
-echo "Public Stage1 Instance State: Ok..."
+echo "Public $BUILDSTAGE Instance State: Ok..."
 echo "Private Instance State: Ok..."
 
 #------------
 # Create IMAGE AMIs
 AMI_IMAGE_PUB1=$(aws ec2 create-image --instance-id $INSTANCE_ID_PUB1 --name $(echo "openvpn-public-1-$INSTANCE_ID_PUB1") --description "Public OpenVPN AMI" --output text)
-echo "Public Stage1 AMI creation has now been initiated : $AMI_IMAGE_PUB1"
+echo "Public $BUILDSTAGE AMI creation has now been initiated : $AMI_IMAGE_PUB1"
 AMI_IMAGE_PRIV=$(aws ec2 create-image --instance-id $INSTANCE_ID_PRIV --name $(echo "openvpn-private-$INSTANCE_ID_PRIV") --description "Private OpenVPN AMI" --output text)
 echo "Private AMI creation has now been initiated : $AMI_IMAGE_PRIV"
 
@@ -109,28 +123,29 @@ P1=$!
 aws ec2 wait image-available --image-ids $AMI_IMAGE_PRIV &
 P2=$!
 wait $P1 $P2
-echo "Public Stage1 AMI is now available : $AMI_IMAGE_PUB1 "
+echo "Public $BUILDSTAGE AMI is now available : $AMI_IMAGE_PUB1 "
 echo "Private AMI is now available : $AMI_IMAGE_PRIV"
 
 # Terminate the instances - no longer needed.
 aws ec2 terminate-instances --instance-ids $INSTANCE_ID_PUB1 $INSTANCE_ID_PRIV > /dev/null
-echo "Stage1 Instances have now terminated..."
+echo "$BUILDSTAGE Instances have now terminated..."
 
 #------------
 # Update Stack with Stage2 parameters
 BUILDSTAGE="Stage2"
+echo "$BUILDSTAGE"
 aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=CurrentAmi,ParameterValue=$AMI_IMAGE_PUB1 --tags Key=Name,Value=openvpn-stage2 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml > /dev/null
-echo "Stage2 Stack Update has now been Initiated..."
+echo "$BUILDSTAGE Stack has now been Initiated..."
 # Wait for Stage2 Update to complete
 if (aws cloudformation wait stack-update-complete --stack-name $STACK_ID)
-then echo "Stage2 Stack Update is now Complete : $STACK_ID"
+then echo "$BUILDSTAGE Stack Update is now Complete : $STACK_ID"
 else echo "Error: Stack Wait Update Failed : $STACK_ID"
 fi
 
 #------------
 # Grab the IDs of the ec2 instances for further processing
 INSTANCE_ID_PUB2=$(aws cloudformation describe-stacks --stack-name $STACK_ID --output text --query "Stacks[].Outputs[?OutputKey == 'InstanceIdPubStage2'].OutputValue")
-echo "Public Stage2 Instance ID is : $INSTANCE_ID_PUB2"
+echo "Public $BUILDSTAGE Instance ID is : $INSTANCE_ID_PUB2"
 
 #------------
 # Validity Check Here.
@@ -138,21 +153,21 @@ echo "Public Stage2 Instance ID is : $INSTANCE_ID_PUB2"
 aws ec2 wait instance-status-ok --instance-ids $INSTANCE_ID_PUB2 &
 P1=$!
 wait $P1
-echo "Public Stage2 Instance State: Ok..."
+echo "Public $BUILDSTAGE Instance State: Ok..."
 
 #------------
 # Create IMAGE AMIs
 AMI_IMAGE_PUB2=$(aws ec2 create-image --instance-id $INSTANCE_ID_PUB2 --name $(echo "openvpn-public-2-$INSTANCE_ID_PUB2") --description "Public 2 OpenVPN AMI" --output text)
-echo "Public Stage2 AMI creation has now been initiated : $AMI_IMAGE_PUB2"
+echo "Public $BUILDSTAGE AMI creation has now been initiated : $AMI_IMAGE_PUB2"
 # Wait for new AMIs to become available
 aws ec2 wait image-available --image-ids $AMI_IMAGE_PUB2 &
 P1=$!
 wait $P1
-echo "Public Stage2 AMI is now available : $AMI_IMAGE_PUB2"
+echo "Public $BUILDSTAGE AMI is now available : $AMI_IMAGE_PUB2"
 
 # Terminate Public Stage2 instance - no longer needed.
 aws ec2 terminate-instances --instance-ids $INSTANCE_ID_PUB2 > /dev/null
-echo "Stage2 Instances have now terminated..."
+echo "$BUILDSTAGE Instances have now terminated..."
 
 #------------
 # Delete AMI + snapshot of Stage1
@@ -162,17 +177,17 @@ echo "Account ID : $AWS_ACC_ID"
 
 # Get snapshot ID Public Stage1 AMI
 SNAPSHOT_PUB1=$(aws ec2 describe-images --image-ids $AMI_IMAGE_PUB1 --query "Images[].BlockDeviceMappings[].Ebs[].SnapshotId" --output text)
-echo "Public Stage1 Snapshot ID : $SNAPSHOT_PUB1"
+echo "Public $BUILDSTAGE Snapshot ID : $SNAPSHOT_PUB1"
 
 # Deregister Public Stage1 image
 if (aws ec2 deregister-image --image-id $AMI_IMAGE_PUB1)
-then echo "Deregistering Public Stage1 AMI : $AMI_IMAGE_PUB1"
-else echo "echo "Error: Failed to Deregister $AMI_IMAGE_PUB1""
+then echo "Deregistering Public $BUILDSTAGE AMI : $AMI_IMAGE_PUB1"
+else echo "Error: Failed to Deregister $AMI_IMAGE_PUB1"
 fi
 
 # Deleting Public Stage1 snapshot
 if (aws ec2 delete-snapshot --snapshot-id $SNAPSHOT_PUB1)
-then echo "Deleting Public Stage1 AMI Snapshot : $SNAPSHOT_PUB1"
+then echo "Deleting Public $BUILDSTAGE AMI Snapshot : $SNAPSHOT_PUB1"
 else echo "Error: Failed to Delete Snapshot $AMI_IMAGE_PUB1"
 fi
 
@@ -180,10 +195,12 @@ fi
 # Create Launch Template for AutoScaling Group
 # Update Stack with Stage3 parameters
 BUILDSTAGE="Stage3"
+echo "$BUILDSTAGE"
 aws cloudformation update-stack --stack-name $STACK_ID --parameters ParameterKey=BuildStage,ParameterValue=$BUILDSTAGE  ParameterKey=CurrentAmi,ParameterValue=$AMI_IMAGE_PUB2 --tags Key=Name,Value=openvpn-stage3 --template-url https://s3.eu-central-1.amazonaws.com/dh.cform-templates/openvpn/dh.cform.openvpn.yaml > /dev/null
-echo "Stage3 Stack Update has now been Initiated..."
+echo "$BUILDSTAGE Stack has now been Initiated..."
 # Wait for Stage3 Update to complete
 if (aws cloudformation wait stack-update-complete --stack-name $STACK_ID)
-then echo "Stage3 Stack Update is now Complete : $STACK_ID"
+then echo "$BUILDSTAGE Stack Update is now Complete : $STACK_ID"
 else echo "Error: Stack Wait Update Failed : $STACK_ID"
 fi
+
--- iptables-scripts/dh.cform.openvpn-ec2-priv-iptables.sh
+++ iptables-scripts/dh.cform.openvpn-ec2-priv-iptables.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 #--
+
 META_NTP="169.254.169.123/32"
 META_DNS="$(grep -m 1 nameserver /etc/resolv.conf | sed s/'nameserver '//)/32"
 LOCAL_IPV4="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)/32"
+PRIV_CIDR="10.0.0.0/17"
 MAC="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)"
 VPC_CIDR="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$MAC"/vpc-ipv4-cidr-block)"
 NIC="$(ip -o -4 route show to default | awk '{print $5}')"
+
 #--
 # Clear ipv4 chains
 iptables -F
@@ -44,36 +47,39 @@ iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 # accept, log and limit incoming pings
 iptables -N IPTlogPASSinICMPreq
-iptables -A INPUT -d $LOCAL_IPV4 -p icmp --icmp-type echo-request -i $NIC -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
+iptables -A INPUT -d $PRIV_CIDR -p icmp --icmp-type echo-request -i $NIC -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
 iptables -A IPTlogPASSinICMPreq -j LOG --log-prefix "IPTlogPASSin__:_PING___:"
 iptables -A IPTlogPASSinICMPreq -j ACCEPT
 # allow and log local outgoing pings
 iptables -N IPTlogPASSoutICMPreq
-iptables -A OUTPUT -s $LOCAL_IPV4 -p icmp --icmp-type echo-request -o $NIC -j IPTlogPASSoutICMPreq
+iptables -A OUTPUT -s $PRIV_CIDR -p icmp --icmp-type echo-request -o $NIC -j IPTlogPASSoutICMPreq
 iptables -A IPTlogPASSoutICMPreq -j LOG --log-prefix "IPTlogPASSout_:_PING___:"
 iptables -A IPTlogPASSoutICMPreq -j ACCEPT
 # allow output local dhcp requests to VPC servers
-iptables -A OUTPUT -s $LOCAL_IPV4 -d $VPC_CIDR -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d $VPC_CIDR -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
 # allow time sync requests to metadata services
-iptables -A OUTPUT -s $LOCAL_IPV4 -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
 # allow dns requests to metadata services
-iptables -A OUTPUT -s $LOCAL_IPV4 -d $META_DNS -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d $META_DNS -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
 # allow output local http/s traffic
 iptables -N IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $LOCAL_IPV4 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $LOCAL_IPV4 -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
 iptables -A IPTlogPASSoutHTTPS -j LOG --log-prefix "IPTlogPASSout_:_HTTPS__:"
 iptables -A IPTlogPASSoutHTTPS -j ACCEPT
 # allow incoming ssh traffic only from VPC CIDR source
 iptables -N IPTlogPASSinSSH
-iptables -A INPUT -s $VPC_CIDR -d $LOCAL_IPV4 -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinSSH
+iptables -A INPUT -s $VPC_CIDR -d $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinSSH
 iptables -A IPTlogPASSinSSH -j LOG --log-prefix "IPTlogPASSin__:_SSH____:"
 iptables -A IPTlogPASSinSSH -j ACCEPT
 # drop and log all unknown traffic
 iptables -A INPUT -j LOG --log-prefix "IPTlogDROPin__:_UNKWN__:"
 iptables -A OUTPUT -j LOG --log-prefix "IPTlogDROPout_:_UNKWN__:"
 iptables -A FORWARD -j LOG --log-prefix "IPTlogDROPfrw_:_UNKWN__:"
+
 # Save rules
 service iptables save
+# /etc/init.d/iptables save
 service ip6tables save
+# /etc/init.d/ip6tables save
 
