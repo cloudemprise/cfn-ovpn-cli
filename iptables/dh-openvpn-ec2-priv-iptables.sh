073c293fe8a87be38e19d48393bf3242d994b357
cloudemprise <dh.info@posteo.net>
2020-05-24 14:08:13 +0000

    Instance Metadata Service Version 2 Token is now required for the Autoscaling Group Launch Template. Email Address used for SNS Notifications is now a script variable and is requested from stdin with relavent error checking. Unique inline Resource policy for S3 Access for Public and Lauch Template Instances.iptables: removed WebServer Input Rules; http input only allowed for network loadbalancer on local subnets.iptables: Instance Metadata Service is now only accessible from local subnets and logged. All other http/s paths are explicitly blocked.

--- iptables/dh-openvpn-ec2-priv-iptables.sh
+++ iptables/dh-openvpn-ec2-priv-iptables.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #--
 
+META_DATA="169.254.169.254"
 META_NTP="169.254.169.123/32"
 META_DNS="$(grep -m 1 nameserver /etc/resolv.conf | sed s/'nameserver '//)/32"
-LOCAL_IPV4="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)/32"
+#LOCAL_IPV4="$(curl -s http://${META_DATA}/latest/meta-data/local-ipv4)/32"
 PRIV_CIDR="10.0.0.0/17"
-MAC="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)"
-VPC_CIDR="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$MAC"/vpc-ipv4-cidr-block)"
+MAC="$(curl -s http://${META_DATA}/latest/meta-data/network/interfaces/macs/)"
+VPC_CIDR="$(curl -s http://${META_DATA}/latest/meta-data/network/interfaces/macs/"$MAC"/vpc-ipv4-cidr-block)"
 NIC="$(ip -o -4 route show to default | awk '{print $5}')"
 
 #--
@@ -47,29 +48,34 @@ iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 # accept, log and limit incoming pings
 iptables -N IPTlogPASSinICMPreq
-iptables -A INPUT -d $PRIV_CIDR -p icmp --icmp-type echo-request -i $NIC -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
+iptables -A INPUT -d $PRIV_CIDR -p icmp --icmp-type echo-request -i "$NIC" -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
 iptables -A IPTlogPASSinICMPreq -j LOG --log-prefix "IPTlogPASSin__:_PING___:"
 iptables -A IPTlogPASSinICMPreq -j ACCEPT
 # allow and log local outgoing pings
 iptables -N IPTlogPASSoutICMPreq
-iptables -A OUTPUT -s $PRIV_CIDR -p icmp --icmp-type echo-request -o $NIC -j IPTlogPASSoutICMPreq
+iptables -A OUTPUT -s $PRIV_CIDR -p icmp --icmp-type echo-request -o "$NIC" -j IPTlogPASSoutICMPreq
 iptables -A IPTlogPASSoutICMPreq -j LOG --log-prefix "IPTlogPASSout_:_PING___:"
 iptables -A IPTlogPASSoutICMPreq -j ACCEPT
 # allow output local dhcp requests to VPC servers
-iptables -A OUTPUT -s $PRIV_CIDR -d $VPC_CIDR -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d "$VPC_CIDR" -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o "$NIC" -j ACCEPT
 # allow time sync requests to metadata services
-iptables -A OUTPUT -s $PRIV_CIDR -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o "$NIC" -j ACCEPT
 # allow dns requests to metadata services
-iptables -A OUTPUT -s $PRIV_CIDR -d $META_DNS -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j ACCEPT
+iptables -A OUTPUT -s $PRIV_CIDR -d "$META_DNS" -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o "$NIC" -j ACCEPT
+# allow Instance Metadata Service from local subnets only
+iptables -N IPTlogPASSoutIMDS
+iptables -A OUTPUT -s $PRIV_CIDR -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o ""$NIC"" -j IPTlogPASSoutIMDS
+iptables -A IPTlogPASSoutIMDS -j LOG --log-prefix "IPTlogPASSout_:_IMDS___:"
+iptables -A IPTlogPASSoutIMDS -j ACCEPT
 # allow output local http/s traffic
 iptables -N IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PRIV_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PRIV_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutHTTPS
 iptables -A IPTlogPASSoutHTTPS -j LOG --log-prefix "IPTlogPASSout_:_HTTPS__:"
 iptables -A IPTlogPASSoutHTTPS -j ACCEPT
 # allow incoming ssh traffic only from VPC CIDR source
 iptables -N IPTlogPASSinSSH
-iptables -A INPUT -s $VPC_CIDR -d $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinSSH
+iptables -A INPUT -s "$VPC_CIDR" -d $PRIV_CIDR -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i "$NIC" -j IPTlogPASSinSSH
 iptables -A IPTlogPASSinSSH -j LOG --log-prefix "IPTlogPASSin__:_SSH____:"
 iptables -A IPTlogPASSinSSH -j ACCEPT
 # drop and log all unknown traffic
--- iptables/dh-openvpn-ec2-pub-iptables.sh
+++ iptables/dh-openvpn-ec2-pub-iptables.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+META_DATA="169.254.169.254"
 META_NTP="169.254.169.123/32"
 META_DNS="$(grep -m 1 nameserver /etc/resolv.conf | sed s/'nameserver '//)/32"
-LOCAL_IPV4="$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)/32"
+#LOCAL_IPV4="$(curl -s http://${META_DATA}/latest/meta-data/local-ipv4)/32"
 PUB_CIDR="10.0.128.0/18"
-MAC="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)"
-VPC_CIDR="$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/"$MAC"/vpc-ipv4-cidr-block)"
+MAC="$(curl -s http://${META_DATA}/latest/meta-data/network/interfaces/macs/)"
+VPC_CIDR="$(curl -s http://${META_DATA}/latest/meta-data/network/interfaces/macs/"$MAC"/vpc-ipv4-cidr-block)"
 VPN_CIDR_UDP="10.100.10.0/24"
 VPN_CIDR_TCP="10.100.20.0/24"
 VPN_CIDR="10.100.0.0/16"
@@ -56,132 +57,132 @@ iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # accept, log and limit incoming pings
 iptables -N IPTlogPASSinICMPreq
-iptables -A INPUT -d $PUB_CIDR -p icmp --icmp-type echo-request -i $NIC -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
+iptables -A INPUT -d $PUB_CIDR -p icmp --icmp-type echo-request -i "$NIC" -m limit --limit 2/s --limit-burst 2 -j IPTlogPASSinICMPreq
 iptables -A IPTlogPASSinICMPreq -j LOG --log-prefix "IPTlogPASSin__:_PING___:"
 iptables -A IPTlogPASSinICMPreq -j ACCEPT
 # allow and log local outgoing pings
 iptables -N IPTlogPASSoutICMPreq
-iptables -A OUTPUT -s $PUB_CIDR -p icmp --icmp-type echo-request -o $NIC -j IPTlogPASSoutICMPreq
+iptables -A OUTPUT -s $PUB_CIDR -p icmp --icmp-type echo-request -o "$NIC" -j IPTlogPASSoutICMPreq
 iptables -A IPTlogPASSoutICMPreq -j LOG --log-prefix "IPTlogPASSout_:_PING___:"
 iptables -A IPTlogPASSoutICMPreq -j ACCEPT
 # accept forwarded ping requests from VPC hosts
 iptables -N IPTlogPASSfrwICMPreq
-iptables -A FORWARD -s $VPN_CIDR -p icmp --icmp-type echo-request -o $NIC -j IPTlogPASSfrwICMPreq
+iptables -A FORWARD -s $VPN_CIDR -p icmp --icmp-type echo-request -o "$NIC" -j IPTlogPASSfrwICMPreq
 iptables -A IPTlogPASSfrwICMPreq -j LOG --log-prefix "IPTlogPASSfrw_:_PING___:"
 iptables -A IPTlogPASSfrwICMPreq -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow output local dhcp requests to VPC servers
 iptables -N IPTlogPASSoutDHCP
-iptables -A OUTPUT -s $PUB_CIDR -d $VPC_CIDR -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutDHCP
+iptables -A OUTPUT -s $PUB_CIDR -d "$VPC_CIDR" -p udp --sport 68 --dport 67 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutDHCP
 iptables -A IPTlogPASSoutDHCP -j LOG --log-prefix "IPTlogPASSout_:_DHCP___:"
 iptables -A IPTlogPASSoutDHCP -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow local ntp sync requests to metadata services
 iptables -N IPTlogPASSoutNTP
-iptables -A OUTPUT -s $PUB_CIDR -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutNTP
-iptables -A IPTlogPASSoutNTP -j LOG --log-prefix "IPTlogPASSout_:_NTP____:"
+iptables -A OUTPUT -s $PUB_CIDR -d $META_NTP -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutNTP
+#iptables -A IPTlogPASSoutNTP -j LOG --log-prefix "IPTlogPASSout_:_NTP____:"
 iptables -A IPTlogPASSoutNTP -j ACCEPT
 # allow forwarded ntp requests from VPN hosts
 iptables -N IPTlogPASSfrwNTP
-iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwNTP
+iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 123 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwNTP
 iptables -A IPTlogPASSfrwNTP -j LOG --log-prefix "IPTlogPASSfrw_:_NTP____:"
 iptables -A IPTlogPASSfrwNTP -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow local dns requests to metadata services
 iptables -N IPTlogPASSoutDNS
-iptables -A OUTPUT -s $PUB_CIDR -d $META_DNS -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutDNS
-iptables -A IPTlogPASSoutDNS -j LOG --log-prefix "IPTlogPASSout_:_DNS____:"
+iptables -A OUTPUT -s $PUB_CIDR -d "$META_DNS" -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutDNS
+#iptables -A IPTlogPASSoutDNS -j LOG --log-prefix "IPTlogPASSout_:_DNS____:"
 iptables -A IPTlogPASSoutDNS -j ACCEPT
 # allow forwarded dns requests from VPN hosts
 iptables -N IPTlogPASSfrwDNS
-iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwDNS
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwDNS
+iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwDNS
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 53 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwDNS
 iptables -A IPTlogPASSfrwDNS -j LOG --log-prefix "IPTlogPASSfrw_:_DNS____:"
 iptables -A IPTlogPASSfrwDNS -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow http requests from any local sources (nlb health checks)
 iptables -N IPTlogPASSinNLB
-iptables -A INPUT -s $PUB_CIDR -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport http -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinNLB
+iptables -A INPUT -s $PUB_CIDR -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport http -m conntrack --ctstate NEW -i "$NIC" -j IPTlogPASSinNLB
 #iptables -A IPTlogPASSinNLB -j LOG --log-prefix "IPTlogPASSin__:_NLB____:"
 iptables -A IPTlogPASSinNLB -j ACCEPT
-# allow http requests from any source except home subnet
-iptables -N IPTlogPASSinHTTPS
-iptables -A INPUT ! -s $PUB_CIDR -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport http -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinHTTPS
-iptables -A IPTlogPASSinHTTPS -j LOG --log-prefix "IPTlogPASSin__:_HTTPS__:"
-iptables -A IPTlogPASSinHTTPS -j ACCEPT
+# allow Instance Metadata Service from local subnets only
+iptables -N IPTlogPASSoutIMDS
+iptables -A OUTPUT -s $PUB_CIDR -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutIMDS
+iptables -A IPTlogPASSoutIMDS -j LOG --log-prefix "IPTlogPASSout_:_IMDS___:"
+iptables -A IPTlogPASSoutIMDS -j ACCEPT
 # allow output local http/s traffic
 iptables -N IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
-iptables -A OUTPUT -s $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PUB_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutHTTPS
+iptables -A OUTPUT -s $PUB_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutHTTPS
 iptables -A IPTlogPASSoutHTTPS -j LOG --log-prefix "IPTlogPASSout_:_HTTPS__:"
 iptables -A IPTlogPASSoutHTTPS -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow forwarded http/s traffic from VPN hosts
 iptables -N IPTlogPASSfrwHTTPS
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwHTTPS
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwHTTPS
+iptables -A FORWARD -s $VPN_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport http  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwHTTPS
+iptables -A FORWARD -s $VPN_CIDR ! -d ${META_DATA}/32 -p tcp --syn --sport 1024:65535 --dport https -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwHTTPS
 iptables -A IPTlogPASSfrwHTTPS -j LOG --log-prefix "IPTlogPASSfrw_:_HTTPS__:"
 iptables -A IPTlogPASSfrwHTTPS -j ACCEPT
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 # block spurious android/chrome tracking udp 443 trafic
 iptables -N IPTlogDROPfrwHTTPS
-iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 443 -m conntrack --ctstate NEW -o $NIC -j IPTlogDROPfrwHTTPS
+iptables -A FORWARD -s $VPN_CIDR -p udp --sport 1024:65535 --dport 443 -m conntrack --ctstate NEW -o "$NIC" -j IPTlogDROPfrwHTTPS
 #iptables -A IPTlogDROPfrwHTTPS -j LOG --log-prefix "IPTlogDROPfrw_:_HTTPS__:"
 iptables -A IPTlogDROPfrwHTTPS -j DROP
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow incoming ssh traffic from any source
 iptables -N IPTlogPASSinSSH
-iptables -A INPUT -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinSSH
+iptables -A INPUT -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport 22 -m conntrack --ctstate NEW -i "$NIC" -j IPTlogPASSinSSH
 iptables -A IPTlogPASSinSSH -j LOG --log-prefix "IPTlogPASSin__:_SSH____:"
 iptables -A IPTlogPASSinSSH -j ACCEPT
 # allow outgoing ssh traffic only within VPC
 iptables -N IPTlogPASSoutSSH
-iptables -A OUTPUT -s $PUB_CIDR -d $VPC_CIDR -p tcp --syn --sport 1024:65535 --dport 22  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSoutSSH
+iptables -A OUTPUT -s $PUB_CIDR -d "$VPC_CIDR" -p tcp --syn --sport 1024:65535 --dport 22  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSoutSSH
 iptables -A IPTlogPASSoutSSH -j LOG --log-prefix "IPTlogPASSout_:_SSH____:"
 iptables -A IPTlogPASSoutSSH -j ACCEPT
 # allow forwarded ssh traffic from VPN hosts
 iptables -N IPTlogPASSfrwSSH
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 22  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwSSH
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 22  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwSSH
 iptables -A IPTlogPASSfrwSSH -j LOG --log-prefix "IPTlogPASSfrw_:_SSH____:"
 iptables -A IPTlogPASSfrwSSH -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow forwarded imap/smtp traffic from VPN hosts
 iptables -N IPTlogPASSfrwMAIL
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 993  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwMAIL
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 465  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwMAIL
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 587  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwMAIL
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 993  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwMAIL
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 465  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwMAIL
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 587  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwMAIL
 iptables -A IPTlogPASSfrwMAIL -j LOG --log-prefix "IPTlogPASSfrw_:_MAIL___:"
 iptables -A IPTlogPASSfrwMAIL -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow forwarded google play store traffic from VPN hosts
 iptables -N IPTlogPASSfrwGPS
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5228  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwGPS
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5228  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwGPS
 iptables -A IPTlogPASSfrwGPS -j LOG --log-prefix "IPTlogPASSfrw_:_GPS____:"
 iptables -A IPTlogPASSfrwGPS -j ACCEPT
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 # allow forwarded (SimCard) Highway Addressable Remote Transducer Protocol traffic from VPN hosts
 iptables -N IPTlogPASSfrwHIP
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5094  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwHIP
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5094  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwHIP
 iptables -A IPTlogPASSfrwHIP -j LOG --log-prefix "IPTlogPASSfrw_:_HART-IP:"
 iptables -A IPTlogPASSfrwHIP -j ACCEPT
 #~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~
 # allow forwarded XMPP vodafone service traffic from VPN hosts
 iptables -N IPTlogPASSfrwXMPP
-iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5222  -m conntrack --ctstate NEW -o $NIC -j IPTlogPASSfrwXMPP
+iptables -A FORWARD -s $VPN_CIDR -p tcp --syn --sport 1024:65535 --dport 5222  -m conntrack --ctstate NEW -o "$NIC" -j IPTlogPASSfrwXMPP
 iptables -A IPTlogPASSfrwXMPP -j LOG --log-prefix "IPTlogPASSfrw_:_XMPP___:"
 iptables -A IPTlogPASSfrwXMPP -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # allow VPN handshake (tcp/udp) requests from any source
 iptables -N IPTlogPASSinVPN
-iptables -A INPUT -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport 1194 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinVPN
-iptables -A INPUT -d $PUB_CIDR -p udp --sport 1024:65535 --dport 1194 -m conntrack --ctstate NEW -i $NIC -j IPTlogPASSinVPN
+iptables -A INPUT -d $PUB_CIDR -p tcp --syn --sport 1024:65535 --dport 1194 -m conntrack --ctstate NEW -i "$NIC" -j IPTlogPASSinVPN
+iptables -A INPUT -d $PUB_CIDR -p udp --sport 1024:65535 --dport 1194 -m conntrack --ctstate NEW -i "$NIC" -j IPTlogPASSinVPN
 iptables -A IPTlogPASSinVPN -j LOG --log-prefix "IPTlogPASSin__:_VPN____:"
 iptables -A IPTlogPASSinVPN -j ACCEPT
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # NAT only VPN traffic
 iptables -t nat -N IPTlogMASQnatPOST
-iptables -t nat -A POSTROUTING -s $VPN_CIDR_UDP -m conntrack --ctstate NEW -o $NIC -j IPTlogMASQnatPOST
-iptables -t nat -A POSTROUTING -s $VPN_CIDR_TCP -m conntrack --ctstate NEW -o $NIC -j IPTlogMASQnatPOST
+iptables -t nat -A POSTROUTING -s $VPN_CIDR_UDP -m conntrack --ctstate NEW -o "$NIC" -j IPTlogMASQnatPOST
+iptables -t nat -A POSTROUTING -s $VPN_CIDR_TCP -m conntrack --ctstate NEW -o "$NIC" -j IPTlogMASQnatPOST
 iptables -t nat -A IPTlogMASQnatPOST -j MASQUERADE
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # drop and log all unknown traffic
